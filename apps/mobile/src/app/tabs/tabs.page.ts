71673c2dcaea0d4a0c89694acc419c06fa51fe43
Helder NORTE <hnorte@solocal.com>
2023-06-22 14:37:30 +0200

    add new Ionic theme

--- apps/mobile/src/app/tabs/tabs.page.ts
+++ apps/mobile/src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'task-ninja-tabs',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
-  constructor() {}
+  constructor(private actionSheetController: ActionSheetController) {}
+
+  async selectAction() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Choose an action',
+      cssClass: 'custom-action-sheet',
+      buttons: [
+        {
+          text: 'Add account',
+          icon: 'wallet',
+          handler: () => {
+            // Put in logic ...
+          },
+        },
+        {
+          text: 'Add transaction',
+          icon: 'swap-horizontal-outline',
+          handler: () => {
+            // Put in logic ...
+          },
+        },
+        {
+          text: 'Set budget',
+          icon: 'calculator',
+          handler: () => {
+            // Put in logic ...
+          },
+        },
+        {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+        },
+      ],
+    });
+    await actionSheet.present();
+  }
 }
