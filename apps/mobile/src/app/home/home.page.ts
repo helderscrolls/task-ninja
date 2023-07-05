2071cc92946d593e798a7937f269a7e17ddba99c
Helder NORTE <hnorte@solocal.com>
2023-07-05 13:24:25 +0200

    feat(home): add initial skeleton for cards

--- apps/mobile/src/app/home/home.page.ts
+++ apps/mobile/src/app/home/home.page.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'task-ninja-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  contentLoaded = false;
+
   constructor() {}
+
+  ngOnInit(): void {
+    // Fake timeout
+    setTimeout(() => {
+      this.contentLoaded = true;
+    }, 2000);
+  }
 }
