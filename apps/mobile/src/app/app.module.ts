be975716fbcd99b6702c07f178a9624509193ba3
Helder NORTE <hnorte@solocal.com>
2023-07-06 12:00:55 +0200

    feat(Firebase): initial setup for firebase

--- apps/mobile/src/app/app.module.ts
+++ apps/mobile/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { IonicModule } from '@ionic/angular';
 
 import { MobileShellModule } from '@task-ninja/mobile/shell/feature';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [IonicModule.forRoot(), MobileShellModule],
+  imports: [IonicModule.forRoot(), MobileShellModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ apps/mobile/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'task-ninja-42158',
+    appId: '1:68101425411:web:25dfdffad9929eedb30136',
+    storageBucket: 'task-ninja-42158.appspot.com',
+    apiKey: 'AIzaSyDohm1cEmYgF_VQlCXlV77oN9qOMKZY3Us',
+    authDomain: 'task-ninja-42158.firebaseapp.com',
+    messagingSenderId: '68101425411',
+    measurementId: 'G-CV1P044RKV',
+  },
+};
