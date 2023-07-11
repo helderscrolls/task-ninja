f6d6d83eaea8a34c9cb7bd55d78d8e1424ec5dbd
Helder NORTE <hnorte@solocal.com>
2023-07-11 16:09:18 +0200

    ci(mobile): add PWA build

--- apps/mobile/src/app/app.module.ts
+++ apps/mobile/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -14,6 +14,7 @@ import { Capacitor } from '@capacitor/core';
 import { MobileShellModule } from '@task-ninja/mobile/shell/feature';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
@@ -32,6 +33,12 @@ import { AppComponent } from './app.component';
       }
     }),
     provideFirestore(() => getFirestore()),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   bootstrap: [AppComponent],
 })
