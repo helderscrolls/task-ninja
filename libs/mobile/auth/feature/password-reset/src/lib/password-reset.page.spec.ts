c5b176de6217a3b3b4a300376bb4b51301404490
Helder NORTE <hnorte@solocal.com>
2023-07-18 10:06:48 +0200

    feat(mobile-auth-feature-password-reset): add reset password logic
    add new resetPassword() to AuthService, connected ResetPassword page to logic, updated form and UTs
    
    #14

--- libs/mobile/auth/feature/password-reset/src/lib/password-reset.page.spec.ts
+++ libs/mobile/auth/feature/password-reset/src/lib/password-reset.page.spec.ts
@@ -1,7 +1,13 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '@task-ninja/mobile/auth/data-access';
 import { PasswordResetPageComponent } from './password-reset.page';
+
+const authServiceMock = {
+  resetPassword: jest.fn(),
+};
 
 describe('PasswordResetPageComponent', () => {
   let component: PasswordResetPageComponent;
@@ -10,7 +16,8 @@ describe('PasswordResetPageComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [PasswordResetPageComponent],
-      imports: [IonicModule.forRoot()],
+      imports: [IonicModule.forRoot(), ReactiveFormsModule],
+      providers: [{ provide: AuthService, useValue: authServiceMock }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PasswordResetPageComponent);
