2814ab55dcd66bb4dec78ce70da60225683be4d1
Helder NORTE <hnorte@solocal.com>
2023-07-19 16:05:25 +0200

    fix(mobile): remove bad fix for service worker

--- apps/mobile/src/app/app.component.ts
+++ apps/mobile/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 import { ToastService } from '@task-ninja/mobile/shared/data-access';
-import { environment } from '../environments/environment';
 
 @Component({
   selector: 'task-ninja-root',
@@ -12,29 +11,27 @@ export class AppComponent implements OnInit {
   constructor(private swUpdate: SwUpdate, private toastService: ToastService) {}
 
   async ngOnInit() {
-    if (environment.production) {
-      this.swUpdate.versionUpdates.subscribe(async (res) => {
-        if (res.type === 'VERSION_READY') {
-          const toast = await this.toastService.updateAvailableToast();
+    this.swUpdate.versionUpdates.subscribe(async (res) => {
+      if (res.type === 'VERSION_READY') {
+        const toast = await this.toastService.updateAvailableToast();
 
-          toast
-            .onDidDismiss()
-            .then(() => this.swUpdate.activateUpdate())
-            .then(() => window.location.reload());
-        }
-      });
-
-      this.swUpdate.checkForUpdate();
-
-      setInterval(() => {
-        this.swUpdate.checkForUpdate();
-      }, 15 * 60 * 1000);
-
-      if (!this.swUpdate.isEnabled) {
-        console.log('swUpdate Enabled ? Nope :(');
-      } else {
-        console.log('swUpdate Enabled ? Yes :)');
+        toast
+          .onDidDismiss()
+          .then(() => this.swUpdate.activateUpdate())
+          .then(() => window.location.reload());
       }
+    });
+
+    this.swUpdate.checkForUpdate();
+
+    setInterval(() => {
+      this.swUpdate.checkForUpdate();
+    }, 15 * 60 * 1000);
+
+    if (!this.swUpdate.isEnabled) {
+      console.log('swUpdate Enabled ? Nope :(');
+    } else {
+      console.log('swUpdate Enabled ? Yes :)');
     }
   }
 }
